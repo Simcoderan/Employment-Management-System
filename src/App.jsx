e77e90ecbc5db2bc6105822f76443c33120a741b
simran <simransingh956429@gmail.com>
2024-10-23 10:17:06 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css';
+import Login from './components/Auth/Login.jsx';
+
+
+const App =() => {
+  return (
+    <>
+     <Login/>
+    </>
+  )
+}
+
+export  default App
+
+
